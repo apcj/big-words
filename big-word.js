b31ba4a5748f037d44134a709d29e5973a6a8e8b
Alistair Jones <alistair.jones@gmail.com>
2014-07-25 22:11:56 +0100

    Support multiple lines of text.

--- big-word.js
+++ big-word.js
@@ -13,21 +13,34 @@
         return context.measureText(text).width;
     }
 
+    function copyStyle(htmlStyle) {
+        return function(selection) {
+            selection
+                .attr('font-family', htmlStyle.getPropertyValue('font-family'))
+                .attr('font-weight', htmlStyle.getPropertyValue('font-weight'));
+        }
+    }
+
     d3.selectAll('div.big-words').each(function() {
         var parent = d3.select(this.parentElement);
         var svg = parent.append('svg');
 
-        var maxSvgWidth = 0;
+        var maxSvgWidth = 1000;
+        var totalHeight = 0;
 
         d3.select(this).selectAll('p.full-width').each(function() {
             var text = d3.select(this);
-            maxSvgWidth = measureText(text.text(), window.getComputedStyle(this, null));
+            var htmlStyle = window.getComputedStyle(this, null);
+            var widthAt100Px = measureText(text.text(), htmlStyle);
+            var fontSize = 100 * maxSvgWidth / widthAt100Px;
+            totalHeight += fontSize;
             svg.append('text')
-                .attr('font-size', '100px')
-                .attr('alignment-baseline', 'central')
-                .text(text.text());
+                .attr('font-size', fontSize + 'px')
+                .call(copyStyle(htmlStyle))
+                .text(text.text())
+                .attr('y', totalHeight);
         });
 
-        svg.attr('viewBox', [0, -50, maxSvgWidth, 100].join(' '));
+        svg.attr('viewBox', [0, 0, maxSvgWidth, totalHeight].join(' '));
     });
 })();
